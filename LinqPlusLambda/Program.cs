520586e104bfb7dcd716023bc1219ad57798e965
Vitor Jansen <vitorvieira.id@gmail.com>
2021-02-08 18:53:50 -0300

    GroupBy: Agrupar determinados valores

--- LinqPlusLambda/Program.cs
+++ LinqPlusLambda/Program.cs
@@ -91,6 +91,18 @@ namespace LinqPlusLambda
             var r15 = products.Where(p => p.Category.Id == 1).Select(p => p.Price).Aggregate(0.0, (x, y) => x + y);
             Console.WriteLine("Category 1 aggregate sum: " + r15);
             Console.WriteLine();
+
+            //GroupBy: Agrupar determinados valores
+            var r16 = products.GroupBy(p => p.Category);
+            foreach (IGrouping<Category, Product> group in r16)
+            {
+                Console.WriteLine("Category " + group.Key.Name + ":");
+                foreach (Product p in group)
+                {
+                    Console.WriteLine(p);
+                }
+                Console.WriteLine();
+            }
         }
 
         static void Print<T>(string message, IEnumerable<T> collection)
